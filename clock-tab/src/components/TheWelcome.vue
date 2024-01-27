bbbdbdd45c067cc75a45944cbd01bafd573d763c
mengzhuou <mou30@gatech.edu>
2024-01-27 17:02:10 -0500

    Added a header

--- clock-tab/src/components/TheWelcome.vue
+++ clock-tab/src/components/TheWelcome.vue
@@ -1,4 +1,13 @@
 <template>
+  <header class="sticky top-0 shadow-lg bg-theme-secondary">
+    <nav class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row">
+      <div class="flex items-center gap-3">
+        <i class="text-2xl fa-solid fa-clock"></i>
+        <p class="text-2xl">Digital Analog Clock</p>
+      </div>
+    </nav>
+
+  </header>
   <div>
     <p>Current Time: {{ currentTime }}</p>
     <p>Current Date: {{ currentDate }}</p>
