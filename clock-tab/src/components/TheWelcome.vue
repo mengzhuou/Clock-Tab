1faf48ce3ac63da0289a92f6b19ccfd760479c5b
mengzhuou <mou30@gatech.edu>
2024-01-28 12:06:56 -0500

    added radio button style
    1. music 星际穿越

--- clock-tab/src/components/TheWelcome.vue
+++ clock-tab/src/components/TheWelcome.vue
@@ -5,8 +5,12 @@
         <i class="text-2xl fa-solid fa-clock"></i>
         <p class="text-2xl">Digital Analog Clock</p>
       </div>
-      <div>
-        <button @click="toggleDarkMode">Dark Mode</button>
+      <div class="flex items-center ml-6">
+        <label class="switch">
+          <input type="checkbox" v-model="darkMode" @click="toggleDarkMode">
+          <span class="slider"></span>
+        </label>
+        <span class="ml-2">{{ darkMode ? 'Dark Mode' : 'Light Mode' }}</span>
       </div>
     </nav>
   </header>
@@ -178,3 +182,64 @@
     ctx.fillText(chosenText, textX, textY);
   }
 </script>
+
+
+<style scoped>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 34px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+  border-radius: 50%;
+}
+
+input:checked + .slider {
+  background-color: #000000;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #ffffff;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.bg-theme-secondary.dark-mode {
+  background-color: #333; 
+  color: #fff; 
+}
+</style>
